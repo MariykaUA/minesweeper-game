4878568d57a97bcbf47f350e7d1c8d5c9846247e
Mariia Hoienko <mariia.hoienko@powercoders.org>
2024-05-03 14:36:19 +0200

    Discover neighbor cells recursively

--- script.js
+++ script.js
@@ -77,17 +77,27 @@ function discoverCell(row, col) {
   // TODO: Task 5 - Reveal cells when clicked.
   //
   if(!cells[row][col].discovered) {
-    cells[row][col].discovered = true;  }
+    cells[row][col].discovered = true; 
 
   //
   // TODO: Task 6 - Discover neighbor cells recursively, as long as there are no adjacent bombs to the current cell.
   //
-
+  if(countAdjacentBombs(row, col) === 0) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+          let newRow = row + i;
+          let newCol = col + j;
+          if (newRow >= 0 && newRow < ROWS_COUNT && newCol >= 0 && newCol < COLS_COUNT) {
+            discoverCell(newRow, newCol);
+          }
+        }
+  }
+}
   //
   // TODO: Task 8 - Implement defeat. If the player "discovers" a bomb (clicks on it without holding shift), set the variable defeat to true.
   //
 }
-
+}
 discoverCell(row, col)
 
 function flagCell(row, col) {
