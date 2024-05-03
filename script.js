f899ccfd938dc1ec86be4e5808456403380a962b
Mariia Hoienko <mariia.hoienko@powercoders.org>
2024-05-03 14:47:39 +0200

    Implement defeat

--- script.js
+++ script.js
@@ -96,6 +96,9 @@ function discoverCell(row, col) {
   //
   // TODO: Task 8 - Implement defeat. If the player "discovers" a bomb (clicks on it without holding shift), set the variable defeat to true.
   //
+  if (cells[row][col].isBomb) {
+    defeat = true;
+  }
 }
 }
 discoverCell(row, col)
@@ -106,7 +109,7 @@ function flagCell(row, col) {
   //                When clicking a cell and holding shift, function flagCell() will be called for you.
   //
   if (cells) {
-    cells[row][col].hasBeenFlagged= !cells[row][col].hasBeenFlaggedFlagged;
+    cells[row][col].hasBeenFlagged = !cells[row][col].hasBeenFlagged;
 
 }
 }
